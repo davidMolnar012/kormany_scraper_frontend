6ba2674dd306fda391d12bf908f7522e81599c5b
Norbert Gocze <gnorbi951@gmail.com>
2021-09-04 20:28:29 +0200

    Refactor request

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
 import Plot from 'react-plotly.js';
 import './App.css';
 
 function App() {
+    axios.interceptors.request.use(function(config: AxiosRequestConfig) {
+        config.baseURL = process.env.REACT_APP_AXIOS_URL
+        return config;
+    });
 
     const [plot, setPlot] = useState<any>();
     const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
-        // http://18.194.102.84:8080/plots
-        axios.get("http://localhost:8080/plots")
+        axios.get('/plots')
         .then((resp) => {
             setPlot(resp.data)
-            console.log(resp.data)
             setLoading(false);
         })
     }, [])
