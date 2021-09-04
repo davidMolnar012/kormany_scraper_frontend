11cc151747829011d9da9843c0bbc637eb1072e6
Norbert Gocze <gnorbi951@gmail.com>
2021-09-04 20:19:06 +0200

    Implement basic display

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Plot from 'react-plotly.js';
 import './App.css';
 
 function App() {
-    return (
+
+    const [plot, setPlot] = useState<any>();
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        // http://18.194.102.84:8080/plots
+        axios.get("http://localhost:8080/plots")
+        .then((resp) => {
+            setPlot(resp.data)
+            console.log(resp.data)
+            setLoading(false);
+        })
+    }, [])
+
+    return(
         <>
-            Hello there
+            {loading ?
+            (
+                <h1>Loading...</h1>
+            )
+            : (
+                <Plot
+                data={plot.data}
+                layout={plot.layout}
+                />
+
+            )}
         </>
     );
 }
