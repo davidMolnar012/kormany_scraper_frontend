ead8074f9aaf94d96207d43d9c206358248d9da6
Norbert Gocze <gnorbi951@gmail.com>
2021-09-19 17:03:06 +0200

    Add provisionary readme

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,10 +1,38 @@
+import "./About.css";
 
 function About() {
     return (
-        <div>
-            about
+        <div className="description-wrapper">
+            <div className="description">
+                <h3>
+                    Szakmai leirás:
+                </h3>
+                <p>
+                    A weboldal mögött egy algoritmus különböző kormány média oldalait olvassa,
+                    majd ezeket összesitve generál egy grafikont az elmúlt napok legemlegetettebb témáiról.
+                </p>
+                <p>
+                    A grafikonon egy egység az y tengelyen 1 emlitést jelent az adott szóból.
+                </p>
+                <p>
+                    Később tervezek hozzáadni egyéb funkcionalitásokat az oldalhoz, jelenleg a szabadidőm a limitáló tényező az oldal fejlődésében.
+                    Kapcsolat: ormis95@gmail.com
+                </p>
+                <h3>
+                    Story:
+                </h3>
+                <p>
+                    Kiakasztó, hogy a kommunikáció a bitek szintjén zajlik (Brüsszel 😠) és ezért itt az ideje
+                    kifigurázni a lakájmédiát és létrehozni egy oldalt a régi foellenseg.hu mintájára,
+                    ami már sajnos évek óta nem üzemel.
+                </p>
+                <p>
+                    A weboldal kezelője, szerkesztője egyik oldallal sem szimpatizál. Az oldal azért jött létre, hogy a politikai kommunikáció
+                    lezüllesztett állapotának tükröt tartson és kifigurázza azt.
+                </p>
+            </div>
         </div>
     )
 }
 
-export default About
+export default About;
