79667d303e751efd54c278225c5d200cc264c6e9
Norbert Gocze <gnorbi951@gmail.com>
2021-09-19 14:18:53 +0200

    Implement basic not found page

--- src/Components/NoutFound/NoutFound.tsx
+++ src/Components/NoutFound/NoutFound.tsx
@@ -1,9 +1,36 @@
 
 function NoutFound() {
     return (
-        <div>
-            Page not found
+        <div
+        style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            marginTop: "20vh",
+            color: "white"
+        }}
+        >
+            <div
+            style={{fontSize: "3rem", marginBottom: "10vh"}}
+            >
+                Ez az oldal nem létezik.
+            </div>
+            <div>
+                <button
+                type="button"
+                onClick={() => window.location.href = "/"}
+                style={{
+                    background: "#ff6b12",
+                    borderRadius: "5px",
+                    color: "white",
+                    fontSize: "1.5rem",
+                    padding: ".8rem",
+                }}
+                >Vissza a főoldalra
+                </button>
+            </div>
         </div>
+
     )
 }
 
